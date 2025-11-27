3a7d038a6de79d302cc90ed417accea6d0cd4ad7
Malin Skaar <maalinskaar@gmail.com>
2025-11-27 17:59:27 +0100

    Missed update on the task3.py. Attempting to fix now.

--- task3.py
+++ task3.py
@@ -2,3 +2,11 @@ from taskoneclassuserinput import UserInputValidator
 
 instances_one = UserInputValidator()
 instances_two = UserInputValidator()
+
+
+num_one = ["2","3","-1","a"]
+num_two = ["fhj","89","-10","1"]
+
+
+check_one = instances_one.display_message(num_one)
+check_one = instances_one.display_message(num_two)
