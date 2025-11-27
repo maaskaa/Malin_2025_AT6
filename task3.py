beea35d511333ec6130e3aecc2ec2f1310e838ce
Malin Skaar <maalinskaar@gmail.com>
2025-11-27 15:09:52 +0100

    Removed question from exam.

--- task3.py
+++ task3.py
@@ -1,10 +1,4 @@
 from taskoneclassuserinput import UserInputValidator
 
-'''. Commit 2: Add an extra Python file that creates two instances of the UserInputValidator class. This
-file should also contain a separate file named extra.txt, which includes the mathematical constant
-π to the 12th digit. This extra.txt file should be ignored for all staging and committing to the online
-repository (see instructions below)'''
-
 instances_one = UserInputValidator()
 instances_two = UserInputValidator()
-
